6406eb4d552d88d0a32ce20567ff8d7763d27529
123123 <zakiroffkam@yandex.ru>
2022-03-30 17:40:00 +0300

    найдена вся инфа для модальных окон

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
--- test.py
+++ test.py
@@ -1,12 +1,48 @@
+# для радио баттон
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import RadioField
+
+SECRET_KEY = 'development'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 
-@app.route('/')
-def test():
-    return render_template('cart.html')
+class SimpleForm(FlaskForm):
+    example = RadioField('Label', choices=[
+        (1, 'description'), (2, 'whatever')],
+                         default=1, coerce=int)
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=['post', 'get'])
+def hello_world():
+    form = SimpleForm()
+    if form.validate_on_submit():
+        print(form.example.data)
+    else:
+        print(form.errors)
+    return render_template('test.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# для чекбоксов
+#
+# from flask import Flask, request
+#
+# app = Flask(__name__)
+#
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         print(request.form.getlist('hello'))
+#
+#     return '''<form method="post">
+# <input type="checkbox" name="hello" value="world" checked>
+# <input type="checkbox" name="hello" value="davidism" checked>
+# <input type="submit">
+# </form>'''
+#
+# app.run()
